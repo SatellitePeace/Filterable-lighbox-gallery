b1253d9e15c7a64747b5eae3542763750c1939be
SatellitePeace <njokunneoma1999@gmail.com>
2025-01-17 18:22:02 +0100

    Fixes and light mode LS addition

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+// DARK - LIGHT MODE TOGGLE
+const darkModeButton = document.querySelector('.lightMode-btn');
+const bodyColor = document.body;
+const isDarkModeEnabled = localStorage.getItem('darkMode') === 'true';
+if (isDarkModeEnabled) {
+  bodyColor.classList.add('dark-mode');
+  darkModeButton.innerHTML = "<i class='bi bi-moon-fill'></i>";
+} else {
+  bodyColor.classList.remove('dark-mode');
+  darkModeButton.innerHTML = "<i class='bi bi-brightness-high-fill'></i>";
+}
+// set darkmode to persist from previous state
+darkModeButton.addEventListener('click', () => {
+  bodyColor.classList.toggle('dark-mode');
+  localStorage.setItem('darkMode', bodyColor.classList.contains('dark-mode'));
+
+  if (bodyColor.classList.contains('dark-mode')) {
+    darkModeButton.innerHTML = "<i class='bi bi-moon-fill'></i>";
+  } else {
+    darkModeButton.innerHTML = "<i class='bi bi-brightness-high-fill'></i>";
+  }
+});
+
 // LIGHTBOX
 const body = document.querySelector("body")
 const lightbox = document.querySelector(".image-gallery-container .lightbox");
@@ -31,47 +54,47 @@ closeBtn.addEventListener("click", () => {
 });
 
 nextBtn.addEventListener("click", () => {
-    if (currentImage.nextElementSibling) {
-      currentImage = currentImage.nextElementSibling;
-    } else {
-      currentImage = currentImage.parentElement.firstElementChild;
-    }
-    showImage(currentImage);
-  });
-  
-  previousBtn.addEventListener("click", () => {
-    if (currentImage.previousElementSibling) {
-      currentImage = currentImage.previousElementSibling;
-    } else {
-      currentImage = currentImage.parentElement.lastElementChild;
-    }
-    showImage(currentImage);
-  });
-  
+  if (currentImage.nextElementSibling) {
+    currentImage = currentImage.nextElementSibling;
+  } else {
+    currentImage = currentImage.parentElement.firstElementChild;
+  }
+  showImage(currentImage);
+});
+
+previousBtn.addEventListener("click", () => {
+  if (currentImage.previousElementSibling) {
+    currentImage = currentImage.previousElementSibling;
+  } else {
+    currentImage = currentImage.parentElement.lastElementChild;
+  }
+  showImage(currentImage);
+});
+
 //   FILTER IMAGES
 const list = document.querySelectorAll(".list")
 const itemBox = document.querySelectorAll(".image")
 
 for (let i = 0; i < list.length; i++) {
-    list[i].addEventListener("click", function () {
-        for (let j = 0; j < list.length; j++) {
-            list[j].classList.remove("active")
-            console.log(1)
-        }
-        this.classList.add("active")
+  list[i].addEventListener("click", function () {
+    for (let j = 0; j < list.length; j++) {
+      list[j].classList.remove("active")
+      // console.log(1)
+    }
+    this.classList.add("active")
 
-        let dataFilter = this.getAttribute("data-name")
+    let dataFilter = this.getAttribute("data-name")
 
-        for (let k = 0; k < itemBox.length; k++) {
-            itemBox[k].classList.remove("active")
-            itemBox[k].classList.add("hide")
+    for (let k = 0; k < itemBox.length; k++) {
+      itemBox[k].classList.remove("active")
+      itemBox[k].classList.add("hide")
 
-            if (itemBox[k].getAttribute("data-name") === dataFilter || dataFilter === "all") {
-                itemBox[k].classList.remove("hide")
-                itemBox[k].classList.add("active")
-            }
-        }
-    })
+      if (itemBox[k].getAttribute("data-name") === dataFilter || dataFilter === "all") {
+        itemBox[k].classList.remove("hide")
+        itemBox[k].classList.add("active")
+      }
+    }
+  })
 }
 // DOWNLOAD IMAGE DIRECTLY FROM THE GALLERY
 itemBox.forEach(item => {
@@ -79,27 +102,17 @@ itemBox.forEach(item => {
   const image = item.querySelector("img");
 
   downloadButton.addEventListener("click", (event) => {
-    event.stopPropagation(); 
+    event.stopPropagation();
     const imageUrl = image.src;
 
     const link = document.createElement("a");
     link.href = imageUrl;
-    link.download = imageUrl.split('/').pop(); 
+    link.download = imageUrl.split('/').pop();
     document.body.appendChild(link);
     link.click();
-    document.body.removeChild(link); 
+    document.body.removeChild(link);
   });
 });
-// LIGHTMODE TOGGLE
-const lightModeBtn = document.querySelector(".lightMode-btn");
-lightModeBtn.onclick = () => {
-  document.body.classList.toggle("dark-mode");
-  console.log(1223)
-};
-// const lightModeBtn = document.querySelector(".lightMode-btn");
-// lightModeBtn.addEventListener("clicks", function(){
-//   console.log("press")
-// })
 
 // COPYRIGHT DATE
 // const copyDate = document.querySelector("span");
